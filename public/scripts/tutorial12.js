4eeea051738ba7667e990c2d9897b93cabc0e05a
Administrator <baxtergu@gmail.com>
2016-05-05 16:35:41 +0800

    Done with tutorial

---
+++ public/scripts/tutorial12.js
@@ -0,0 +1,20 @@
+var CommentBox = React.createClass({
+    getInitialState: function(){
+        return {data:[]};
+    },
+    
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox data={data} />,
+    document.getElementById('content')
+);
